eb81b7189c34762c3b1f2461e98f5df6fb701959
Wakio37 <your_email@example.com>
2023-10-27 23:37:32 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+    (1, 'Spooks', 5, 'M', 'Brown', 'Friendly', true),
+    (2, 'Rowdy', 3, 'M', 'Black', 'Playful', true),
+    (3, 'Tabitha', 4, 'F', 'Golden', 'Shy', true),
+    (4, 'Sergeant Brown', 7, 'M', 'Brown', 'Brave', true),
+    (5, 'Melissa', 2, 'F', 'White', 'Energetic', true),
+    (6, 'Grinch', 10, 'M', 'Green', 'Grumpy', true),
+    (7, 'Wendy', 6, 'F', 'Blue', 'Friendly', true),
+    (8, null, 1, 'M', 'Gray', 'Unknown', false);
